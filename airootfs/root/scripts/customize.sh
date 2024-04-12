4dbb99ef438a64ed63956730ced45ff02e43fb6c
Votre Nom <tom@p113.io>
2024-04-12 11:45:26 +0200

    first commit

---
+++ airootfs/root/scripts/customize.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+chmod +x start.sh
+chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+chmod +x start.sh
+chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+chmod +x start.sh
+chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+sudo chmod +x start.sh
+sudo chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+sudo chmod +x start.sh
+sudo chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
+#!/usr/bin/env bash
+yay -Sy --noconfirm git
+git clone https://github.com/tombombadilom/AI-Local-Machine.git
+cd AI-Local-Machine
+sudo chmod +x start.sh
+sudo chmod +x install/*.sh
+./start.sh full
+cd install && ./install-arch-full.sh
+./hyprland-end-install.sh
+./app-install.sh
+echo " end script "
